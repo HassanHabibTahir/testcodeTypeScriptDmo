1f356e3be525d6881ed053a2e8d3f4475564a081
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2020-12-25 10:34:23 +0500

    TYPE-SCRIPT-TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainApp  from './Routes/mainApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <MainApp/>
     </div>
   );
 }
 
 export default App;
+
+
+// Test Artical LInks get json file and test
+//https://feralamillo.medium.com/create-react-app-typescript-testing-with-jest-and-enzyme-869fdba1bd3
+//get testing Componets
+//https://www.toptal.com/react/tdd-react-user-stories-to-development
